567244b78772237a2fe662dcc0f4a8931fb3676a
Raphael LAZZARI--ARMOUR <63423645+RaphLAZ@users.noreply.github.com>
2021-03-12 11:08:24 +0100

    Add files via upload

---
+++ IN_405/TD/TD4/main.c
@@ -0,0 +1,29 @@
+#include "internals.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pwd.h>
+#include <dirent.h>
+#include <errno.h>
+
+int main(int argc, char **argv){
+  struct file* origin;
+  printf("adresse mémoire origin:  %p\n",origin);
+
+
+
+  /*if (argc != 3){
+    printf("Bad usage: %s src dest\n"
+           "   - src : source directory\n"
+           "   - dest: phantom directory\n", argv[0]);
+    return EXIT_FAILURE;
+  }*/
+  browse_directory(argv[1],&origin);
+  printf("parent->name %s\n",origin->name);
+
+    return 0;
+}
