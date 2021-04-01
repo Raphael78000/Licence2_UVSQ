2971ccc316dc36a47d2208299d5e084bc868d0a8
RaphLAZ <raphaelarmourlazzari@gmail.com>
2021-04-01 09:54:50 +0200

    ajout

--- IN_405/TD/TD5/Q2.c
+++ IN_405/TD/TD5/Q2.c
@@ -13,19 +13,18 @@ void fils(int a){
 }
 
 int main(int argc,char** argv){
-	int i;
 	pid_t a;
 
 	srand(time(NULL));
 
-	for (i = 0; i < 10; ++i){
+	for (int i = 0; i < 10; ++i){
 		// Génération dans le processus pere pour sortir les nombres de la
 		// même graine
 		int r = 1+(rand()%10);
 		if (!fork()) fils(r);
 	}
 
-	for (i = 0; i < 10; ++i){
+	for (int i = 0; i < 10; ++i){
 		a = wait(NULL);
 		printf("%d est fini\n",a);
 	}
--- IN_405/TD/TD5/Q3.c
+++ IN_405/TD/TD5/Q3.c
@@ -5,39 +5,37 @@
 #include <time.h>
 #include <unistd.h>
 
-void question1(int m, int n){
+void question1(int m,int n){
 	int i,j,t = 1,status;
 
-	for (i = 0; i < m; ++i){
-		for (j = 0; j < n; ++j){
+	for (i = 0;i < m; ++i){
+		for (j = 0;j < n; ++j){
 			if (!fork()) exit(t);
 		}
 	}
 
-	for (i = 0; i < m; ++i){
-		for (j = 0; j < n; ++j){
+	for (i = 0;i < m; ++i){
+		for (j = 0;j < n; ++j){
 			wait(&status);
 			t += WEXITSTATUS(status);
 		}
 	}
-
 	printf("Nombre de processus créés : %d!\n",t);
 }
 
-void question2(int m, int n){
-	int i, j, t = 1, status;
+void question2(int m,int n){
+	int i,j,t = 1,status;
 
-	for (i = 0; i < m; ++i){
+	for (i = 0;i < m; ++i){
 		if (fork()) continue;
-		for (j = 0; j < n; ++j){
+		for (j = 0;j < n; ++j){
 			if (!fork()) exit(t);
 		}
 
-		for (j = 0; j < n; ++j){
+		for (j = 0;j < n; ++j){
 			wait(&status);
 			t += WEXITSTATUS(status);
 		}
-
 		exit (t);
 	}
 
@@ -50,12 +48,12 @@ void question2(int m, int n){
 }
 
 // Version itérative de la question 3
-void question3(int m, int n){
+void question3(int m,int n){
 	pid_t a;
-	int i, j, t = 1, status;
+	int i,j,t = 1,status;
 
-	for (i = 0; i < m; ++i){
-		for (j = 0; j < n; ++j){
+	for (i = 0;i < m; ++i){
+		for (j = 0;j < n; ++j){
 			// Si fils, ne créé pas d'autres processus à cette étape
 			if (!(a = fork())) break;
 		}
@@ -63,7 +61,7 @@ void question3(int m, int n){
 		// Si père
 		if (a){
 			// Attente des fils
-			for (j = 0; j < n; ++j){
+			for (j = 0;j < n; ++j){
 				wait(&status);
 				t += WEXITSTATUS(status);
 			}
@@ -84,15 +82,15 @@ void question3(int m, int n){
 // Version récursive de la question 3
 // firstCall est vrai lors de l'entrée de la fonction récursive, puis faux :
 // utilisé pour l'affichage par le proc principal
-void question3R(int m, int n, int firstCall){
-	int i, t = 1, status;
+void question3R(int m,int n,int firstCall){
+	int i,t = 1,status;
 
 	if (!m) exit(t);
 
-	for (i = 0; i < n; ++i)
+	for (i = 0;i < n; ++i)
 		if (!fork()) question3R(m-1, n, 0);
 
-	for (i = 0; i < n; ++i){
+	for (i = 0;i < n; ++i){
 		wait(&status);
 		t += WEXITSTATUS(status);
 	}
